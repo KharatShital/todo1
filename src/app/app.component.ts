590961c08693b2f4a3721245f91deec8970c153a
CA-SHITALKHARAT <kharatshital59@gmail.com>
2024-02-02 10:39:38 +0530

    add searching functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  list:any[]=[]
+  filteredList: any[] = []; 
+  searchTerm: string = '';
+
+  addname(item:string){
+    console.log(item)
+    this.list.push({id:this.list.length+1,username:item})
+    console.log(this.list)
+  }
+  capitalName(item:any){
+    item.username = item.username.toUpperCase();
 }
+
+searchNames() {
+  this.filteredList = this.list.filter(item =>
+    item.username.toLowerCase().includes(this.searchTerm.toLowerCase())
+  );
+}
+}
+
