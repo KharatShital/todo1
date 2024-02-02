20f358f6cfb523049f3c890ab22bfcefa9a25ddb
CA-SHITALKHARAT <kharatshital59@gmail.com>
2024-02-02 11:20:05 +0530

    add name and serach it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
-  list:any[]=[]
-  filteredList: any[] = []; 
+  list: any[] = []
   searchTerm: string = '';
+  //   filteredList: any[] = []; 
 
-  addname(item:string){
-    console.log(item)
-    this.list.push({id:this.list.length+1,username:item})
-    console.log(this.list)
+  
+  addname(item: string) {
+    console.log(item);
+    this.list.push({ id: this.list.length + 1, username: item });
+    console.log(this.list);
+    this.searchTerm = '';
   }
-  capitalName(item:any){
+  
+  capitalName(item: any) {
     item.username = item.username.toUpperCase();
+  }
+
+  searchNames() {
+      return this.list.filter(item =>
+      item.username.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 }
 
-searchNames() {
-  this.filteredList = this.list.filter(item =>
-    item.username.toLowerCase().includes(this.searchTerm.toLowerCase())
-  );
-}
-}
+
 
